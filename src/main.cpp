01cc2d1ab8a183d66a791e5c8007f239b92a1e51
CameronD35 <camster09@ca.rr.com>
2025-11-26 19:09:31 -0800

    Initialization.

---
+++ src/main.cpp
@@ -0,0 +1,172 @@
+#include <Arduino.h>
+#include <SD.h>
+#include <SPI.h>
+// #include <SdFat.h>
+
+#define SD_CS_PIN 10
+
+class DualSD {
+
+  private:
+  
+    File teensySDFile;
+    File externalSDFile;
+
+    char* teensyFileName;
+    char* externalSDFileName;
+
+    int teensy_cspin;
+    int external_cspin;
+
+  public:
+
+    DualSD(char* teensyFileName, char* externalSDFileName) {
+
+      teensySDFile = NULL;
+      externalSDFile = NULL;
+
+      teensy_cspin = NULL;
+      external_cspin = NULL;
+
+      this->teensyFileName = teensyFileName;
+      this->externalSDFileName = externalSDFileName;
+
+    }
+
+    int begin(int teensy_cspin, int external_cspin) {
+
+      if (SD.begin(teensy_cspin) && SD.begin(external_cspin)) {
+        
+        this->teensy_cspin = teensy_cspin;
+        this->external_cspin = external_cspin;
+
+        return 1;
+      
+      }
+
+      return -1;
+    
+    };
+
+    // int open(char* teensyFileName, char* externalSDFileName) {
+
+
+    //   // if the cs pins are defined we know that SD.begin() has been called
+    //   if (teensy_cspin && external_cspin) {
+        
+    //     teensySDFile = SD.open(teensyFileName, FILE_WRITE);
+    //     externalSDFile = SD.open(externalSDFileName, FILE_WRITE);
+
+    //     if (teensySDFile && externalSDFile) { return 1; }
+
+    //     // indicates SD has begun but files could not be opened for writing
+    //     return 0;
+
+    //   }
+      
+    //   // indicates that SD.begin() has not been called
+    //   return -1;
+
+    // }
+
+    // int close() {
+
+    //   // if the cs pins are defined we know that SD.begin() has been called
+    //   if (teensy_cspin && external_cspin) {
+
+    //     teensySDFile.close();
+    //     externalSDFile.close();
+
+    //     return 1;
+
+    //   }
+
+    //   return -1;
+
+    // }
+
+    int exists(char* filename) {
+
+      SD.exists(filename);
+
+    };
+
+    int initializeFiles() {
+
+      // get current time
+      // format to something nice
+      // create file and have starter CSV line
+
+    };
+
+    int write(char* data) {
+
+      // ensure teensy sd card is selected and external sd is not
+      // https://forum.arduino.cc/t/two-sd-cards-on-the-same-project/1012605
+      digitalWrite(external_cspin, HIGH);
+      digitalWrite(teensy_cspin, LOW);
+
+      teensySDFile = SD.open(teensyFileName, FILE_WRITE);
+      teensySDFile.write(data);
+      teensySDFile.close();
+
+      // ensure external sd card is selected and teensy sd is not
+      digitalWrite(teensy_cspin, HIGH);
+      digitalWrite(external_cspin, LOW);
+
+      externalSDFile = SD.open(externalSDFileName, FILE_WRITE);
+      externalSDFile.write(data);
+      externalSDFile.close();
+
+    }
+  
+};
+
+const DualSD SDManager;
+
+// put function declarations here:
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(19200);
+
+  Serial.println("Testing MicroSD");
+
+  if (SD.begin(SD_CS_PIN)) {
+
+    Serial.println("MicroSD Extension working.");
+
+  } else {
+
+    Serial.println("MicroSD Extension not working.");
+
+  }
+
+
+
+}
+
+void loop() {
+
+  
+  // put your main code here, to run repeatedly:
+
+  // digitalWrite(LED_BUILTIN, HIGH);
+
+  // Serial.println("LED on.");
+
+  // delay(500);
+
+  // digitalWrite(LED_BUILTIN, LOW);
+
+  // Serial.println("LED off.");
+
+  // delay(500);
+
+}
+
+// put function definitions here:
